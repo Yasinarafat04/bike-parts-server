fbf07f96228f18aee4d59d854419b9480a5170df
yasinarafat04 <arafattt05@gmail.com>
2022-06-29 15:05:53 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Bike Accessories Server Side Code
+## Md Tomiz Uddin
+
+
+## Used Dependencies
+- ### Node
+- ### Express
+- ### Mongoose
+- ### Body-parser
+- ### Nodemailer
+- ### Stripe
+- ### Cors
+
+## Available Scripts
+
+In the project directory, you can run:
+### For Development
+    npm startDev
+
+### For Deployment
+    npm start
---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// used Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3qtbx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// Create a async fucntion to all others activity
+async function run() {
+    try {
+        await client.connect();
+
+        // Create Database to store Data
+        const serviceCollection = client.db("bike-parts").collection("services");
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+
+    } finally {
+        // await client.close();
+    }
+}
+
+// Call the fuction you decleare abobe
+run().catch(console.dir);
+
+
+// Root Api to cheack activity
+app.get("/", (req, res) => {
+    res.send("Welcome to bike parts!");
+});
+
+app.use('/users', require('./Routes/usersRouter'))
+app.use('/product', require('./Routes/productRoute'))
+app.use('/order', require('./Routes/orderRouter'))
+app.use('/review', require('./Routes/reviewsRouter'))
+app.use('/profile', require('./Routes/profileRouter'))
+app.use('/payment', require('./Routes/paymentRouter'))
+
+app.listen(port, () => {
+    console.log(`bike parts listening on port ${port}`);
+});
